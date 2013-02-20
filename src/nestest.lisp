9741ccee439b795d2a44fc2f8a3e00798d86a2da
Brit Butler <redline6561@gmail.com>
2013-02-20 13:17:14 -0500

    Fix nestest bugs. Reset is blowing up on CCL, investigate later.

--- src/nestest.lisp
+++ src/nestest.lisp
@@ -1,8 +1,10 @@
 (in-package :famiclom)
 
 (defun init-testing ()
-  (reset *nes*)
-  (load-rom (app-path "tests/nestest.nes")))
+  ;(reset *nes*)
+  (setf *nes* (make-nes))
+  (load-rom (app-path "tests/nestest.nes"))
+  (setf (6502::cpu-pc (nes-cpu *nes*)) #xc000))
 
 (defmethod log-state ((cpu cpu))
   (with-accessors ((ar 6502::cpu-ar)
@@ -17,11 +19,8 @@
   ;; Py65, SprocketNES, and TENES all use different vals for the CPUs reset state.
   ;; Just appease the nestest expectations in this case.
   (with-accessors ((cpu nes-cpu)) *nes*
-    (setf (6502::cpu-sp cpu) #xfd
-          (6502::cpu-sr cpu) #x24)
     (with-open-file (in (app-path "tests/nestest.log"))
       (loop for line = (read-line in nil) while line
-         do (let ((step (6502::6502-step cpu (get-byte (6502::immediate cpu)))))
-              (unless (search (log-state cpu) line)
-                (format t "BUG: Got ~A, expected ~%~A~%" (log-state cpu) line)
-                (return t)))))))
+         if (search (log-state cpu) line)
+         do (6502:6502-step cpu (get-byte (6502::immediate cpu)))
+         else return (format t "BUG: Got ~A, expected ~%~A~%" (log-state cpu) line)))))
