ed9c58a3d30e6ad439f08693029f0c6f35759f46
Brit Butler <redline6561@gmail.com>
2013-02-22 23:11:27 -0500

    First pass at input handling.

--- src/input.lisp
+++ src/input.lisp
@@ -1,20 +1,59 @@
 (in-package :famiclom)
 
+(defenum strobe-state (:a :b :select :start :up :down :left :right))
+(defenum keymap ((:sdl-key-a       :a)
+                 (:sdl-key-b       :b)
+                 (:sdl-key-space   :select)
+                 (:sdl-key-return  :start)
+                 (:sdl-key-up      :up)
+                 (:sdl-key-down    :down)
+                 (:sdl-key-left    :left)
+                 (:sdl-key-right   :right)))
+
+(deftype bool () '(member t nil))
+
+(defstruct pad
+  (buttons (make-array 8 :element-type 'bool :initial-element nil))
+  (strobe :a :type keyword))
+
+(defvar *pad* (make-pad) "An input device to retrieve commands from.")
+
+(defgeneric get-state (pad)
+  (:documentation "Get the current state of PAD.")
+  (:method ((pad pad))
+    (aref (pad-buttons pad) (%strobe-state (pad-strobe pad)))))
+
+(defgeneric next-state (pad)
+  (:documentation "Update the strobe value of PAD.")
+  (:method ((pad pad))
+    (with-accessors ((strobe pad-strobe)) pad
+      (setf strobe (%strobe-state strobe :next)))))
+
+(defgeneric check-input (pad)
+  (:documentation "Check for input from the user.")
+  (:method (pad)
+    (sdl:with-events ()
+      (:quit-event () :quit)
+      (:key-down-event (:key key)
+        (case key
+          (:sdl-key-escape :quit)
+          (t (setf (aref (pad-buttons pad) (keymap key)) t))))
+      (:idle () nil))))
+
+(defgeneric handle-input (key down)
+  (:documentation "Update the KEY state based on DOWN.")
+  (:method (key down) nil))
+
 (defun get-byte-input% (addr)
   (if (= addr #x4016)
-      (prog1 'get-gamepad-state 'next-state)
+      (prog1
+          (get-state *pad*)
+        (next-state *pad*))
       0))
 
 (defun (setf get-byte-input%) (new-val addr)
+  ; TODO: Should this just be (reset *gamepad*)?
   (when (= addr #x4016)
-    'reset-state))
+    (setf (pad-strobe *pad*) :a)))
 
-(defvar *gamepad* nil
-  "An input device to retrieve commands from.")
-
-(defgeneric next-key (device)
-  (:documentation "Get the next keypress from DEVICE."))
-
-;; Note: A RESET method is part of the gamepad interface.
-;; The GF is already imported from cl-6502.
-
+; TODO: (sdl:enable-key-repeat 10 10)?
