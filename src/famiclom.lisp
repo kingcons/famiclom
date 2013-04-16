725e36298603b7ab8750e1277d7e2cb17c028993
Brit Butler <redline6561@gmail.com>
2013-04-16 18:42:20 -0400

    Give memory utils their own file, misc cleanups.

--- src/famiclom.lisp
+++ src/famiclom.lisp
@@ -12,40 +12,6 @@
 (defvar *nes* (make-nes))
 (defparameter *debug* nil)
 
-(defun lsb (num)
-  "Gets the least significant byte of an int or bit of a byte."
-  (etypecase num
-    (u8 (ldb (byte 1 0) num))
-    (u16 (ldb (byte 8 8) num))))
-
-(defun msb (num)
-  "Gets the most significant byte of an int or bit of a byte."
-  (etypecase num
-    (u8 (ldb (byte 1 7) num))
-    (u16 (ldb (byte 8 8) num))))
-
-(defun get-byte-ram% (addr)
-  (aref (nes-ram *nes*) (logand addr #x7ff)))
-
-(defun (setf get-byte-ram%) (new-val addr)
-  (setf (aref (nes-ram *nes*) (logand addr #x7ff)) new-val))
-
-(defun 6502:get-byte (addr)
-  (cond ((< addr #x2000) (get-byte-ram% addr))
-        ((< addr #x4000) (get-byte-ppu% addr))
-        ((< addr #x4018) (get-byte-input% addr))
-        (t (get-mapper (nes-mapper *nes*) addr))))
-
-(defun (setf 6502:get-byte) (new-val addr)
-  (cond ((< addr #x2000) (setf (get-byte-ram% addr) new-val))
-        ((< addr #x4000) (setf (get-byte-ppu% addr) new-val))
-        ((< addr #x4018) (setf (get-byte-input% addr) new-val))
-        (t (set-mapper (nes-mapper *nes*) addr new-val))))
-
-(defun 6502:get-range (start end)
-  (coerce (loop for i from start to (1- end)
-             collecting (6502:get-byte i)) 'vector))
-
 (defmethod 6502-step :before ((cpu cpu) opcode)
   (when *debug* (current-instruction cpu t)))
 
@@ -67,11 +33,11 @@
 (defun run ()
   (with-accessors ((cpu nes-cpu)
                    (ppu nes-ppu)) *nes*
-    (loop do
-         (let ((c-step (6502-step cpu (6502:get-byte (6502:immediate cpu))))
-               (p-step (ppu-step ppu (6502:cpu-cc cpu))))
-           (when (ppu-result-vblank p-step)
-             (6502:nmi cpu))
-           (when (ppu-result-new-frame p-step)
-             (sdl:update-display)
-             (get-input *pad*))))))
+    (loop
+       (let ((c-step (6502-step cpu (get-byte (6502:immediate cpu))))
+             (p-step (ppu-step ppu (6502:cpu-cc cpu))))
+         (when (ppu-result-vblank p-step)
+           (6502:nmi cpu))
+         (when (ppu-result-new-frame p-step)
+           (sdl:update-display)
+           (get-input *pad*))))))
---
+++ src/mem.lisp
@@ -0,0 +1,62 @@
+(in-package :famiclom)
+
+(defun 6502:get-byte (addr)
+  (cond ((< addr #x2000) (get-byte-ram% addr))
+        ((< addr #x4000) (get-byte-ppu% addr))
+        ((< addr #x4018) (get-byte-input% addr))
+        ((< addr #x8000) (format t "Cartridge RAM not yet implemented"))
+        (t (get-mapper (nes-mapper *nes*) addr))))
+
+(defun (setf 6502:get-byte) (new-val addr)
+  (cond ((< addr #x2000) (setf (get-byte-ram% addr) new-val))
+        ((< addr #x4000) (setf (get-byte-ppu% addr) new-val))
+        ((< addr #x4018) (setf (get-byte-input% addr) new-val))
+        ((< addr #x8000) (format t "Cartridge RAM not yet implemented"))
+        (t (set-mapper (nes-mapper *nes*) addr new-val))))
+
+(defun 6502:get-range (start end)
+  (coerce (loop for i from start to (1- end)
+             collecting (6502:get-byte i)) 'vector))
+
+(defun wrap-ram (addr)
+  "Wrap ADDR to index the CPU's RAM."
+  (logand addr #x7ff))
+
+(defun wrap-nametable (addr)
+  "Wrap ADDR to index into the PPU nametable."
+  (logand addr #x07ff))
+
+(defun wrap-palette (addr)
+  "Wrap ADDR to index into the PPU palette."
+  (logand addr #x1f))
+
+(defun get-byte-ram% (addr)
+  (aref (nes-ram *nes*) (wrap-ram addr)))
+
+(defun (setf get-byte-ram%) (new-val addr)
+  (setf (aref (nes-ram *nes*) (wrap-ram addr)) new-val))
+
+(defun chr-slice (rom n &optional (size #x1000))
+  "Given a ROM and page, n, return the Nth 4kb chunk of sprite data (CHR)."
+  (let ((start (* #x1000 n)))
+    (subseq (rom-chr rom) start (+ start size))))
+
+(defun wrap-bank (addr)
+  "Wrap an address, ADDR, to a 16k ROM bank."
+  (logand addr #x3fff))
+
+(defun high-bank-p (addr)
+  "Is ADDR accessing the high ROM bank (i.e. > #xc000)."
+  (logbitp 14 addr))
+
+(defun lsb (num)
+  "Gets the least significant byte of an int or bit of a byte."
+  (etypecase num
+    (u8 (ldb (byte 1 0) num))
+    (u16 (ldb (byte 8 8) num))))
+
+(defun msb (num)
+  "Gets the most significant byte of an int or bit of a byte."
+  (etypecase num
+    (u8 (ldb (byte 1 7) num))
+    (u16 (ldb (byte 8 8) num))))
